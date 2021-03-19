f1510a9a9c857f7cac5f641929cfc050ea44f99b
Enni Dahlström <n0daen00@students.oamk.fi>
2021-03-19 12:27:55 +0200

    Navbaria siistitty vähän

--- src/App.js
+++ src/App.js
@@ -1,38 +1,46 @@
 import './App.css';
+import logo from "./toimistologo.png";
 
 function App() {
   return (
     <main>
       <header>
-        <div></div>
         {/* Navbar */}
-          <nav class="navbar navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">
-              <img src="#" width="30" height="30" class="d-inline-block align-top" alt=""/>
-                Verkkokauppa
-            </a>
-          </nav>
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav nav justify-content-center">
-            <li class="nav-item">
-              <a class="nav-link" href="#">kategoria</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">kategoria</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">kategoria</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">kategoria</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        {/*Logo ja otsikko*/}
+        <nav className="justify-content-center navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">
+            <img src={logo} width="30" height="30" className="d-inline-block align-top" alt=""/>&nbsp;&nbsp;
+              Verkkokauppa
+          </a>
+        </nav>
+   
+        {/* Kategoriat */}
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+          <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="justify-content-center collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Toimistotarvikkeet</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Huonekalut</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Elektroniikka</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      
+            <p className="kirjautuminen">
+              <a className="nav-link" href="#">Kirjaudu sisään</a>
+              <a className="nav-link" href="#">Rekisteröidy</a>
+              <a className="nav-link" href="#"><i class="fa fa-shopping-cart" alt="ostoskori" aria-hidden="true"></i></a>
+            </p>
       </header>
 
     {/* Sisältö */}
