52ef871cb4bc01f3e29157358dd1f219ffed3edb
Mikael Niemi <n0nimi00@students.oamk.fi>
2021-03-23 13:01:32 +0200

    Osat komponenteiksi

--- src/App.js
+++ src/App.js
@@ -4,7 +4,72 @@ import logo from "./toimistologo.png";
 function App() {
   return (
     <main>
-      <header>
+      <Header/>
+        
+      
+
+
+      {/* Sisältö */}
+      <article>
+      <Kuvapalkki />
+
+      <section id="kuvaus">
+        <h3>Asiaa meistä</h3>
+          <p>Verkkokauppamme tarjoaa suuren valikoiman erilaisia toimistotarvikkeita
+            tarpeisiinne. Ostosten tekeminen on helppoa ja nopeaa sekä vaivatonta. Toivomme, että
+            löydätte tarvitsemanne.
+          </p>
+      </section>
+      </article>
+      <Footer/>
+    </main>
+  );
+}
+
+function Kuvapalkki() {
+  return (
+    <div id="tuotepalkki" className="carousel carousel-dark slide" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="/Nitoja.png" className="d-block mx-auto" width="200" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src="/pöytälamppu.png" className="d-block mx-auto" width="200" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src="/lehtikotelo.png" className="d-block mx-auto" width="200" alt="..."/>
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#tuotepalkki" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Edellinen</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#tuotepalkki" data-bs-slide="next">
+        <span className="carousel-control-next-icons" aria-hidden="true"></span>
+        <span className="visually-hidden">Seuraava</span>
+      </button>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="bg-dark">
+        <h3>Tekijät:</h3>
+          <div id="tekijat">
+              <li>Enni Dahlström</li>
+              <li>Mikael Niemi</li>
+              <li>Alina Zinchenko</li>
+              <li>Niko Rantonen</li>
+              <li>Jukka Keränen</li>
+          </div>
+    </footer>
+  );
+}
+
+function Header() {
+  return (
+    <header>
         {/* Navbar */}
         {/*Logo ja otsikko*/}
         <nav className="justify-content-center navbar navbar-dark bg-dark">
@@ -55,62 +120,6 @@ function App() {
 
      
       </header>
-        
-      
-
-
-    {/* Sisältö */}
-    <article>
-    <Kuvapalkki />
-
-    <section id="kuvaus">
-      <h3>Asiaa meistä</h3>
-        <p>Verkkokauppamme tarjoaa suuren valikoiman erilaisia toimistotarvikkeita
-          tarpeisiinne. Ostosten tekeminen on helppoa ja nopeaa sekä vaivatonta. Toivomme, että
-          löydätte tarvitsemanne.
-        </p>
-    </section>
-    </article>
-
-    <footer className="bg-dark">
-      <h3>Tekijät:</h3>
-        <div id="tekijat">
-            <li>Enni Dahlström</li>
-            <li>Mikael Niemi</li>
-            <li>Alina Zinchenko</li>
-            <li>Niko Rantonen</li>
-            <li>Jukka Keränen</li>
-        </div>
-    </footer>
-
-    </main>
   );
 }
-
-function Kuvapalkki() {
-  return (
-    <div id="tuotepalkki" className="carousel carousel-dark slide" data-bs-ride="carousel">
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src="/Nitoja.png" className="d-block mx-auto" width="200" alt="..."/>
-        </div>
-        <div className="carousel-item">
-          <img src="/pöytälamppu.png" className="d-block mx-auto" width="200" alt="..."/>
-        </div>
-        <div className="carousel-item">
-          <img src="/lehtikotelo.png" className="d-block mx-auto" width="200" alt="..."/>
-        </div>
-      </div>
-      <button className="carousel-control-prev" type="button" data-bs-target="#tuotepalkki" data-bs-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Edellinen</span>
-      </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#tuotepalkki" data-bs-slide="next">
-        <span className="carousel-control-next-icons" aria-hidden="true"></span>
-        <span className="visually-hidden">Seuraava</span>
-      </button>
-    </div>
-  );
-}
-
 export default App;
