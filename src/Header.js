244de895d7ddfd9d184f5856f314c958c8685fda
Enni Dahlström <n0daen00@students.oamk.fi>
2021-03-24 10:28:30 +0200

    Navbar-osuus erilliseen Header.js-tiedostoon

---
+++ src/Header.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+import logo from "./toimistologo.png";
+
+export default function Header({url}) {
+
+
+    return (
+        <header>
+            {/* Navbar */}
+            {/*Logo ja otsikko*/}
+            <nav className="justify-content-center navbar navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">
+                <img src={logo} width="30" height="30" className="d-inline-block align-top" alt=""/>&nbsp;&nbsp;
+                Tepon Toimistotavarat
+            </a>
+            </nav>
+        
+        {/* Kirjautuminen, rekisteröityminen, ostoskori */}
+        <div className="d-flex flex-row-reverse row p-2 bg-light">
+            <div className="pt-2 col-12 col-xl-4">
+            <a id="kirjautuminen" href="#">Kirjaudu sisään</a>&nbsp;/&nbsp;<a id="kirjautuminen" href="#">Rekisteröidy</a>
+            <a href="#"><i className="fa fa-shopping-cart px-3" alt="ostoskori" aria-hidden="true"></i></a>
+            </div>
+            
+        {/* Hakupalkki */}
+        <div className="input-group rounded col-12 col-xl-4 col-md px-4 py-2 mx-auto" id="haku">
+            <input type="search" className="form-control rounded" placeholder="Hae tuotteita..."
+            aria-describedby="search-addon" />
+            <span className="input-group-text border-0" id="tuotehaku">
+            <i className="fa fa-search"></i>
+            </span>
+        </div>
+
+        {/* Kategoriat */}
+            <nav className="navbar navbar-expand-sm navbar-light bg-light col-12 col-xl-4 col-md">
+                <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">TOIMISTOTARVIKKEET</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">HUONEKALUT</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">ELEKTRONIIKKA</a>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+            </div>
+
+        
+        </header>
+    );
+      
+}
