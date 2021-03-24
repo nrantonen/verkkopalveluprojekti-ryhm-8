770f9adced833bc12fc58a94f573053e2d35b526
Mikael Niemi <n0nimi00@students.oamk.fi>
2021-03-24 11:06:04 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,57 +1,26 @@
+import { Switch, Route} from 'react-router-dom';
 import './App.css';
 import logo from "./toimistologo.png";
 import Tuotesivu from './tuotesivu.js';
+import Ryhma from './tuoteryhmäsivu';
+import Etusivu from './etusivu';
 
 function App() {
   return (
     <main>
       <Header/>
+      <Switch>
+        <Route path="/" component={Etusivu} exact/>
+        <Route path="/tuoteryhmäsivu" component={Ryhma} />
+      </Switch>
         
       
-
-
-      {/* Sisältö */}
-      <article>
-      <Kuvapalkki />
-
-      <section id="kuvaus">
-        <h3>Asiaa meistä</h3>
-          <p>Verkkokauppamme tarjoaa suuren valikoiman erilaisia toimistotarvikkeita
-            tarpeisiinne. Ostosten tekeminen on helppoa ja nopeaa sekä vaivatonta. Toivomme, että
-            löydätte tarvitsemanne.
-          </p>
-      </section>
-      </article>
       <Footer/>
     </main>
   );
 }
 
-function Kuvapalkki() {
-  return (
-    <div id="tuotepalkki" className="carousel carousel-dark slide" data-bs-ride="carousel">
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src="/Nitoja.png" className="d-block mx-auto" width="200" alt="..."/>
-        </div>
-        <div className="carousel-item">
-          <img src="/pöytälamppu.png" className="d-block mx-auto" width="200" alt="..."/>
-        </div>
-        <div className="carousel-item">
-          <img src="/lehtikotelo.png" className="d-block mx-auto" width="200" alt="..."/>
-        </div>
-      </div>
-      <button className="carousel-control-prev" type="button" data-bs-target="#tuotepalkki" data-bs-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Edellinen</span>
-      </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#tuotepalkki" data-bs-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Seuraava</span>
-      </button>
-    </div>
-  );
-}
+
 
 function Footer() {
   return (
@@ -105,7 +74,7 @@ function Header() {
               <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                   <li className="nav-item">
-                    <a className="nav-link" href="#">TOIMISTOTARVIKKEET</a>
+                    <a className="nav-link" href="/tuoteryhmäsivu">TOIMISTOTARVIKKEET</a>
                   </li>
                   <li className="nav-item">
                     <a className="nav-link" href="#">HUONEKALUT</a>
