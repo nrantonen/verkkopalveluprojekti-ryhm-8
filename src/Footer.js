abb6021c7db76195c1bf0d6c40f891317af9c75b
Alina Zinchenko <c0zial00@students.oamk.fi>
2021-04-07 11:43:47 +0300

    Yllapito css

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <footer className="bg-dark">
-            <div className="inline">
+        <footer className="bg-dark row">
+            <div className="col-12 col-md-6">
                 <h3>Tekijät:</h3>
                     <div id="tekijat">
                         <li>Enni Dahlström</li>
@@ -13,8 +13,8 @@ export default function Footer() {
                         <li>Jukka Keränen</li>
                     </div>
             </div>
-            <div>
-                <a id="yllapito" href="/Yllapito">Ylläpito</a>
+            <div className="col-12 col-md-6">
+                <a id="yllapitolinkki" href="/Yllapito">Ylläpito</a>
             </div>
         </footer>
     );
--- src/Yllapito.js
+++ src/Yllapito.js
@@ -3,14 +3,20 @@ import './App.css';
 
 export default function Yllapito() {
     return(
-        <section className="yll_section">
-            <div>
+        <section className="yll_loginform row">
+            <div className="col-12">
                 <h5>Kirjaudu ylläpitäjänä</h5>
             </div>
-            <div>
+            <div className="col-12">
             <form action={URL + "#"} method="POST">
-                <div><input type="text" placeholder="Sähköpostiosoite" name="yll_email" maxLength="30" required /></div>
-                <div><input type="password" placeholder="Salasana" name="yll_salasana" maxLength="30" required /></div>
+                <div className="yll_loginrivi">
+                    <label>Sähköpostiosoite:</label><br/>
+                    <input type="text" placeholder="Sähköpostiosoite" name="yll_email" maxLength="30" required />
+                </div>
+                <div className="yll_loginrivi">
+                    <label>Salasana:</label><br/>
+                    <input type="password" placeholder="Salasana" name="yll_salasana" maxLength="30" required />
+                </div>
                 <div><input type="submit" value="Kirjaudu sisään" /></div>
             </form>
             </div>
