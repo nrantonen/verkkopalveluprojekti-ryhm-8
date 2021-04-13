cfa7729f2bea3272cd883cc8a4086ea3f484a027
Enni Dahlström <n0daen00@students.oamk.fi>
2021-04-13 12:22:31 +0300

    Tuotetietojen muokkaus

--- src/Tuotemuokkaus.js
+++ src/Tuotemuokkaus.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { useLocation } from 'react-router';
+// import {Link} from 'react-router-dom';
 
 
 export default function Tuotemuokkaus({url}) {
@@ -22,30 +23,37 @@ export default function Tuotemuokkaus({url}) {
         alert(error);
       }
     }, []);
+
     
     return (
-        <section>
-        <form action="tallennaTuoteMuokkaus.php" method="post">                    
+        <section> 
+          {/* Tuotteen tietojen muokkaaminen toimii näin, mutta reititys keskeneräinen. */}
+        <form action={url + "products/editProduct.php"} method="post">                    
             <h2>Muokkaa tuotteen #{product.tuotenro} tietoja</h2>
                 <div>
-                    <label>Tuotenimi</label>
-                    <input type="hidden" name="tuotenro"
-                        value={product.tuotenimi}/>
+                <input type="hidden" name="tuotenro" defaultValue={product.tuotenro}></input>
                 </div>
                 <div>
                     <label>Tuotenimi</label>
-                    <input type="hidden" name="tuotenro"
-                        value={product.tuotenimi}/>
-                </div><div>
-                    <label>Tuotenimi</label>
-                    <input type="hidden" name="tuotenro"
-                        value={product.tuotenimi}/>
-                </div><div>
-                    <label>Tuotenimi</label>
-                    <input type="hidden" name="tuotenro"
-                        value={product.tuotenimi}/>
+                    <input type="text" name="tuotenimi"
+                        defaultValue={product.tuotenimi}></input>
                 </div>
-                <input type="submit" value="Vahvista muokkaukset" />
+                <div>
+                    <label>Hinta</label>
+                    <input type="text" name="hinta"
+                        defaultValue={product.hinta}></input>
+                </div>
+                <div>
+                    <label>Kuvaus</label>
+                    <textarea type="text" name="kuvaus"
+                        defaultValue={product.kuvaus}></textarea>
+                </div>
+                <div>
+                    <label>Tuoteryhmä</label>
+                    <input type="text" name="trnro"
+                        defaultValue={product.trnro}></input>
+                </div>
+                <input type="submit" value="Vahvista muokkaukset"/>
         </form>
       </section>
 )
