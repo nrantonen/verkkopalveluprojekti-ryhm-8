10867eb668c8e0ec46c2884b7a89b34cac88d8f1
Enni Dahlström <n0daen00@students.oamk.fi>
2021-04-24 14:52:22 +0300

    Yll_etusivu

--- src/Yll_etusivu.js
+++ src/Yll_etusivu.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Redirect, Link} from 'react-router-dom';
 
-export default function Yll_etusivu() {
+
+export default function Yll_etusivu({yllapito}) {
+
+    if(yllapito === null) {
+        return <Redirect to="/Yllapito" />
+    }
+    
+
     return (
         <div>
-            <p>Ylläpidon etusivu</p>
+            <Link to="/Yll_logout">Kirjaudu ulos</Link>
+            <h2>Ylläpidon etusivu</h2>
+            <div>
+                <Link to="/MuokkaaTuotteita"><h6>Hallinnoi tuotteita</h6></Link>
+            </div>
         </div>
     )
 }
